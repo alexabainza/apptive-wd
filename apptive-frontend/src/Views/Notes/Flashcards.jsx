da9a06cfc713ef2fc39bd47b80b781f8aa98ee70
alex.abainza <abainza.alexandra@gmail.com>
2023-12-20 23:42:56 +0800

    final add card

--- apptive-frontend/src/Views/Notes/Flashcards.jsx
+++ apptive-frontend/src/Views/Notes/Flashcards.jsx
@@ -1,12 +1,12 @@
-// FlashcardPage.jsx
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
 import UserNavbar from "../Dashboard/UserNavbar";
 
-const FlashcardPage = ({ highlightedTextQuestion, highlightedTextAnswer }) => {
+const FlashcardPage = () => {
   const { folder_name, note_id } = useParams();
   const [isFlipped, setFlipped] = useState(false);
   const [flashcards, setFlashcards] = useState([]);
+  const [currentCardIndex, setCurrentCardIndex] = useState(0);
   const storedToken = localStorage.getItem("token");
   const navigate = useNavigate();
 
@@ -19,9 +19,9 @@ const FlashcardPage = ({ highlightedTextQuestion, highlightedTextAnswer }) => {
       .then((response) => response.json())
       .then((data) => {
         if (data != null) {
-          setFlashcards(data.data);
+          setFlashcards(data.data || []);
         } else {
-          console.error("Error fetching flashcards:", data.message);
+          console.error("Error fetching flashcards:", data?.message);
         }
       })
       .catch((error) => {
@@ -33,8 +33,47 @@ const FlashcardPage = ({ highlightedTextQuestion, highlightedTextAnswer }) => {
     setFlipped(!isFlipped);
   };
 
+  const handleNextCard = () => {
+    setCurrentCardIndex((prevIndex) => (prevIndex + 1) % flashcards.length);
+    setFlipped(false); // Reset flip state when moving to the next card
+  };
+
+  const handlePrevCard = () => {
+    setCurrentCardIndex(
+      (prevIndex) => (prevIndex - 1 + flashcards.length) % flashcards.length
+    );
+    setFlipped(false); // Reset flip state when moving to the previous card
+  };
+
   const handleGoToNotes = () => {
     navigate(`../${folder_name}/${note_id}`);
+  };
+
+  const handleDeleteFlashcard = async (flashcardId) => {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/${folder_name}/${note_id}/flashcards/${flashcardId}/delete`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: storedToken,
+          },
+        }
+      );
+
+      if (response.ok) {
+        // Flashcard deleted successfully, update the local state
+        setFlashcards((prevFlashcards) =>
+          prevFlashcards.filter(
+            (flashcard) => flashcard.flashcard_id !== flashcardId
+          )
+        );
+      } else {
+        console.error("Failed to delete flashcard:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error deleting flashcard:", error);
+    }
   };
 
   return (
@@ -53,28 +92,40 @@ const FlashcardPage = ({ highlightedTextQuestion, highlightedTextAnswer }) => {
         <div>
           <div className="flashcard mx-5">
             <div className="flashcard-content" onClick={handleFlip}>
-              {flashcards && flashcards.length > 0 ? (
-                flashcards.map((flashcard, index) => (
-                  <div
-                    key={index}
-                    className={isFlipped ? "back" : "front"}
-                  >
-                    {isFlipped ? flashcard.answer : flashcard.question}
+              {flashcards.length > 0 ? (
+                isFlipped ? (
+                  <div className="back">
+                    {flashcards[currentCardIndex].answer}
                   </div>
-                ))
+                ) : (
+                  <div className="front">
+                    {flashcards[currentCardIndex].question}
+                  </div>
+                )
               ) : (
                 <div className="no-flashcards-message">
                   You have no flashcards.
                 </div>
               )}
             </div>
-            {flashcards && flashcards.length > 0 && (
+            {flashcards.length > 0 && (
               <div className="button-container">
-                {/* You can add your navigation buttons here if needed */}
+                <button onClick={handlePrevCard} className="nav-button">
+                  &lt; Prev
+                </button>
+                <div className="card-index">Card {currentCardIndex + 1}</div>
+                <button onClick={handleNextCard} className="nav-button">
+                  Next &gt;
+                </button>
               </div>
             )}
           </div>
         </div>
+        {flashcards.length > 0 && (
+          <button onClick={() => handleDeleteFlashcard(flashcards[currentCardIndex].flashcard_id)}>
+            Delete
+          </button>
+        )}
       </div>
     </div>
   );
